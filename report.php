5038394d118b03cbfe9ed2f723ce31c10750ae9b
atiflovely999 <74484457+atiflovely999@users.noreply.github.com>
2020-11-29 16:34:43 +0200

    Add files via upload

---
+++ report.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body background="t.jpg">
+     <br>
+     <br>
+     <br>
+     <center>
+         <font color="brown"  size="15px">
+<a href="admin.php"><h1>الرجوع الى شاشه الدخول</h1></a>
+</font>
+</center>
+    <center>
+ <font color="red "  size=20px>
+<h1>  بسم الله الرحمن الرحيم</h1>
+<h1> في هذه الصفحه يتحكم بالمكتبه</h1>
+<h1>ويوجد فيها التقاير وعدد الزوار  </h1>
+
+    </center>
+</body>
+</html>
