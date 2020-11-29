8dea0d631c5feb6a16212fc361e7ec308bbcec99
atiflovely999 <74484457+atiflovely999@users.noreply.github.com>
2020-11-29 12:23:04 +0200

    Update 1.php

--- 1.php
+++ 1.php
@@ -117,15 +117,15 @@
             
             
             <right>  <div class="topnav">
-              <a class="active" href="index.html"><h1>الرئيسية</a></h1>
-              <a class="active" href="islamic.html"><h1>كتب اسلاميه</a></h1>
-              <a class="active" href="Medicine.html"><h1>كتب الطب</a></h1>
-              <a class="active" href="engineering.html"><h1>كتب هندسه</a></h1>
-              <a class="active" href="low.html"><h1>كتب قانون</a></h1>
-              <a class="active" href="mankind.html"><h1>كتب تنميه بشريه</a></h1>
-              <a class="active" href="computer.html"><h1>كتب حاسوب</a></h1>
-              <a class="active" href="novels.html"><h1>روايات</a></h1>
-              <a class="active" href="art.html"><h1>كتب ادب</a></h1>
+              <a class="active" href="index.php"><h1>الرئيسية</a></h1>
+              <a class="active" href="islamic.php"><h1>كتب اسلاميه</a></h1>
+              <a class="active" href="Medicine.php"><h1>كتب الطب</a></h1>
+              <a class="active" href="engineering.php"><h1>كتب هندسه</a></h1>
+              <a class="active" href="low.php"><h1>كتب قانون</a></h1>
+              <a class="active" href="mankind.php"><h1>كتب تنميه بشريه</a></h1>
+              <a class="active" href="computer.php><h1>كتب حاسوب</a></h1>
+              <a class="active" href="novels.php"><h1>روايات</a></h1>
+              <a class="active" href="art.php"><h1>كتب ادب</a></h1>
               </div> </right>
 
 <tr>
