15578f0ae78876f44eca7aa3bda98da0141e9326
atiflovely999 <74484457+atiflovely999@users.noreply.github.com>
2020-11-29 17:54:20 +0200

    Update art.php

--- art.php
+++ art.php
@@ -121,7 +121,6 @@ img {vertical-align: middle;}
           <a class="active" href="engineering.php"><h1>كتب هندسه</a></h1>
           <a class="active" href="low.php"><h1>كتب قانون</a></h1>
           <a class="active" href="mankind.php"><h1>كتب تنميه بشريه</a></h1>
-          <a class="active" href="computer.php"><h1>كتب حاسوب</a></h1>
           <a class="active" href="1.php"><h1>المصممين</a></h1>
           </div> </right>
           <br>
